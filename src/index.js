7503f2a57ab8b5f2462de3ea67cc6201e7fb6a6a
William Ford <wford1.home@gmail.com>
2021-03-15 16:20:00 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const container = document.querySelector('#restaurant');
+
+const content = document.createElement('h1');
+content.classList.add('content');
+content.textContent = 'test';
+container.appendChild(content);
+
