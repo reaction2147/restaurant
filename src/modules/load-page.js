8043a86cc18b09789fc500a34676fe0ebede96da
William Ford <wford1.home@gmail.com>
2021-03-16 12:10:03 +0900

    added menu, contact , and home modules

--- src/modules/load-page.js
+++ src/modules/load-page.js
@@ -1,25 +1,74 @@
+// Create navigation
+
 function createNav(id) {
-    const nav = document.createElement("nav");
-    nav.setAttribute("id", id);
-    const homeBtn = createBtn("home", "home");
-    nav.appendChild(homeBtn);
-    return nav;
-  }
-  
-  function createBtn(id, text) {
-    const button = document.createElement("button");
-    button.setAttribute("id", id);
-    const span = document.createElement("span");
-    span.textContent = text;
-    button.appendChild(span);
-    button.classList.add("tab");
-    return button;
-  }
-  
-  function createPage() {
-    const container = document.getElementById("container");
-    const navBar = createNav("nav");
-    container.appendChild(navBar);
-  }
-  
-  export default createPage;
+  const nav = document.createElement("nav");
+  nav.setAttribute("id", id);
+  const homeBtn = createBtn("home", "home");
+  const menuBtn = createBtn("menu", "menu");
+  const contactBtn = createBtn("contact", "contact");
+  nav.appendChild(homeBtn);
+  nav.appendChild(menuBtn);
+  nav.appendChild(contactBtn);
+  return nav;
+}
+
+//Create buttons for navigation
+
+function createBtn(id, text) {
+  const button = document.createElement("button");
+  button.setAttribute("id", id);
+  const span = document.createElement("span");
+  span.textContent = text;
+  button.appendChild(span);
+  button.classList.add("tab");
+  return button;
+}
+
+//Create header
+
+function createHeader(id, text) {
+  const header = document.createElement("header");
+  header.setAttribute("id", id);
+  const title = document.createElement("h1");
+  title.textContent = text;
+  header.appendChild(title);
+  return header;
+}
+
+// Create area from main content from tab buttons
+
+function createMain(id) {
+  const main = document.createElement("main");
+  main.setAttribute("id", id);
+  return main;
+}
+
+//Create the footer
+function createFooter(id, text) {
+  const footer = document.createElement('footer');
+  footer.setAttribute("id", id);
+  const footerText = document.createElement('h3');
+  footerText.textContent = text;
+  footer.appendChild(footerText);
+  return footer;
+}
+
+//Create the page
+function createPage() {
+  // taget container
+  const container = document.getElementById("container");
+  //create title
+  const title = createHeader("header", "This is some awesome Title");
+  container.appendChild(title);
+  //create nav
+  const navBar = createNav("nav");
+  container.appendChild(navBar);
+  //creat tab content
+  const tabContent = createMain("tab-content");
+  container.appendChild(tabContent);
+  //create footer
+  const footer = createFooter("footer", "Created by Nippon Arts");
+  container.appendChild(footer);
+}
+
+export default createPage;
