0570cfcf04bf109099dbf727e6e5f4af86009756
William Ford <wford1.home@gmail.com>
2021-03-17 19:05:39 +0900

    Added menu

--- src/modules/menu-content.js
+++ src/modules/menu-content.js
@@ -1,9 +1,9 @@
 function setBtnActive(id) {
-    // remove active state from button
+  // remove active state from button
   const activeBtn = document.querySelector(".tab.active");
   if (activeBtn) activeBtn.classList.remove("active");
 
-    // add active state to button
+  // add active state to button
   const homeBtn = document.getElementById(id);
   homeBtn.classList.add("active");
 }
@@ -13,11 +13,15 @@ function loadMenu() {
   const content = document.getElementById("tab-content");
   content.classList.add("grid-layout");
   content.textContent = "";
-  const menu = createMenu();
+  const menu = createMenu(
+   'https://starwarsblog.starwars.com/wp-content/uploads/2018/07/ackbar-ice-cream-tall.jpg',
+    'Alien Fast Food',
+    '20 Specels',
+    'Alien Fast Food')
   content.appendChild(menu);
 }
 
-function createMenu() {
+function createMenu(src, text, price, alt) {
   const section = document.createElement("section");
   section.classList.add("section");
 
@@ -26,6 +30,27 @@ function createMenu() {
   title.textContent = "MENU";
   section.appendChild(title);
 
+  const img = document.createElement("image");
+  
+  img.setAttribute("src", src);
+  img.setAttribute("alt", alt);
+
+  const description = document.createElement("div");
+  description.classList.add("description");
+
+  const foodName = document.createElement("h3");
+  foodName.textContent = text;
+
+  const foodPrice = document.createElement("h2");
+  foodPrice.textContent = price;
+
+  description.appendChild(foodName);
+  description.appendChild(foodPrice);
+
+  section.appendChild(img);
+  section.appendChild(description);
+  
+  
   return section;
 }
 
